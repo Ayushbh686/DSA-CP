e9ac08ee86a999feb37b78f56ff1cd195e91b6d5
Ayushbh686 <Ayushbh686@gmail.com>
2024-12-15 23:11:48 +0530

    DSU

---
+++ DSA/Disjoint Set Union/DSU.cpp
@@ -0,0 +1,57 @@
+//DSU impleentation by rank with path compression 
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int find(vector<int>& parent , int x){  //tc = log*n
+    if(parent[x] == x) return x;
+    return parent[x] = find(parent , parent[x])  ;
+}
+
+void Union(vector<int>& parent , vector<int>& rank , int a , int b){  // tc = log*n
+    //cout<<"entered union function"<<endl;
+    int parA = find(parent , a);
+    //cout<<"fount parent of a in union command"<<endl;
+    int parB = find(parent , b);
+    //cout<<"fount parent of b in union command"<<endl;
+    if(rank[parA] >= rank[parB]){
+        rank[parA]++;
+        parent[parB] = parA;
+        //cout<<"updated rank and parent in Union"<<endl;
+    }
+    else{
+        rank[parB]++;
+        parent[parA] = parB;
+        //cout<<"updated rank and parent in Union"<<endl;
+    }
+}
+
+int main(){
+    int n , m;
+    //n->elements , m -> queries
+    cin>>n>>m;
+
+    vector<int> parent(n+1);
+    vector<int> rank(n+1 , 0);
+
+    for(int i=0 ; i<=n ; i++){
+        parent[i] = i;
+    }
+
+    while(m--){
+        string str;
+        cin>>str;
+        //cout<<str<<endl;
+        if(str == "Union"){
+            int x , y;
+            cin>>x>>y;
+            Union(parent , rank , x , y);
+        }
+        else if(str == "find"){
+            int x;
+            cin>>x;
+            cout<<find(parent  ,x)<<"\n";
+        }
+    }
+}
