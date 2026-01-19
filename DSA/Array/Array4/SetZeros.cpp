6bec3c816aea7a747c6a354ed98079988179a76d
Ayushbh686 <Ayushbh686@gmail.com>
2026-01-20 01:07:57 +0530

    some changes

--- DSA/Array/Array4/SetZeros.cpp
+++ DSA/Array/Array4/SetZeros.cpp
@@ -1,63 +1,50 @@
 class Solution {
 public:
 
-    void setRow(vector<vector<int>>& matrix , int i){
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        for(int c=0 ; c<n ; c++){
-            matrix[i][c] = 0;
+    void setRowZero(vector<vector<int>>& matrix , int i){
+        for(int j=0 ; j<matrix[0].size() ; j++){
+            matrix[i][j] = 0;
         }
+        return;
+    }
 
-        return ;
-    } 
-
-    void setCol(vector<vector<int>>& matrix , int j){
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        for(int r=0 ; r<m ; r++){
-            matrix[r][j] = 0;
+    void setColZero(vector<vector<int>>& matrix , int i){
+        for(int j=0 ; j<matrix.size() ; j++){
+            matrix[j][i] = 0;
         }
-
-        return ;
-    } 
+        return;
+    }
 
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        int col0 = 1; //from col0 with j=1 to j=n-1 will give us the coloumn to be set zero and i=0 to i = m-1 will give row to set 0
+        bool isFirstRowZero = false;
+        bool isFirstColZero = false;
 
-        for(int i=0 ; i<m ; i++){
-            for(int j=0 ; j<n ; j++){
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
                 if(matrix[i][j] == 0){
+                    if(i==0) isFirstRowZero = true;
+                    if(j==0) isFirstColZero = true;
                     matrix[i][0] = 0;
-                    if(j != 0){
-                        matrix[0][j] = 0;
-                    }
-                    else col0 = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
 
-        for(int i=0 ; i<m ; i++){
-            for(int j=0 ; j<n ; j++){
-                cout<<matrix[i][j]<<" ";
-            }
-            cout<<endl;
+        for(int i=1 ; i<n ; i++){
+            if(matrix[i][0] == 0) setRowZero(matrix , i);
         }
 
-
-        for(int j=1 ; j<n ; j++){
-            if(matrix[0][j] == 0) setCol(matrix , j);
+        for(int i=1 ; i<m ; i++){
+            if(matrix[0][i] == 0) setColZero(matrix , i);
         }
 
-        for(int i=0 ; i<m ; i++){
-            if(matrix[i][0] == 0) setRow(matrix , i);
-        }
-        
-        if(col0 == 0) setCol(matrix , 0);
+        if(isFirstRowZero) setRowZero(matrix , 0);
+        if(isFirstColZero) setColZero(matrix , 0);
 
-        return ;
+        return;
+
     }
 };
