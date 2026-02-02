b19d7aac07c9b72a512bb0f7a5c4ae0f3521e570
Ayushbh686 <Ayushbh686@gmail.com>
2026-02-02 22:04:52 +0530

    count inversion

--- DSA/Merge Sort/CountInversion.cpp
+++ DSA/Merge Sort/CountInversion.cpp
@@ -1,74 +1,58 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-
-int merge(vector<int> &arr, int low, int mid, int high) {
-    vector<int> temp; // temporary array
-    int left = low;      // starting index of left half of arr
-    int right = mid + 1;   // starting index of right half of arr
-
-    //Modification 1: cnt variable to count the pairs:
-    int cnt = 0;
-
-    //storing elements in the temporary array in a sorted manner//
-
-    while (left <= mid && right <= high) {
-        if (arr[left] <= arr[right]) {
-            temp.push_back(arr[left]);
-            left++;
+class Solution {
+  public:
+  
+    int MergeAndCount(int lo , int mid , int hi , vector<int>&arr){
+        vector<int>help(hi-lo+1);
+        int i=lo , j=mid+1 ,  k=0 , count = 0;
+        
+        while(i <= mid and j <= hi){
+            if(arr[i] <= arr[j]){
+                help[k] = arr[i];
+                k++;
+                i++;
+            }
+            else{
+                help[k] = arr[j];
+                k++;
+                j++;
+                count += (mid - i + 1);
+            }
         }
-        else {
-            temp.push_back(arr[right]);
-            cnt += (mid - left + 1); //Modification 2
-            right++;
+        
+        while(i <= mid){
+            help[k] = arr[i];
+            k++;
+            i++;
         }
+        
+        while(j <= hi){
+            help[k] = arr[j];
+            k++;
+            j++;
+        }
+        
+        for(int i=lo ; i<=hi ; i++){
+            arr[i] = help[i-lo];
+        }
+        
+        return count;
+        
     }
-
-    // if elements on the left half are still left //
-
-    while (left <= mid) {
-        temp.push_back(arr[left]);
-        left++;
+  
+    int MergeSort(int lo , int hi , vector<int> &arr){
+        int count = 0;
+        if(lo >= hi) return count;
+        
+        int mid = (lo + hi)/2;
+        
+        count += MergeSort(lo , mid , arr);
+        count += MergeSort(mid+1 , hi , arr);
+        count += MergeAndCount(lo , mid , hi , arr);
+        
+        return count;
     }
-
-    //  if elements on the right half are still left //
-    while (right <= high) {
-        temp.push_back(arr[right]);
-        right++;
+  
+    int inversionCount(vector<int> &arr) {
+        return MergeSort(0 , arr.size() - 1 , arr);
     }
-
-    // transfering all elements from temporary to arr //
-    for (int i = low; i <= high; i++) {
-        arr[i] = temp[i - low];
-    }
-
-    return cnt; // Modification 3
-}
-
-int mergeSort(vector<int> &arr, int low, int high) {
-    int cnt = 0;
-    if (low >= high) return cnt;
-    int mid = (low + high) / 2 ;
-    cnt += mergeSort(arr, low, mid);  // left half
-    cnt += mergeSort(arr, mid + 1, high); // right half
-    cnt += merge(arr, low, mid, high);  // merging sorted halves
-    return cnt;
-}
-
-int numberOfInversions(vector<int>&a, int n) {
-
-    // Count the number of pairs:
-    return mergeSort(a, 0, n - 1);
-}
-
-int main()
-{
-    vector<int> a = {5, 4, 3, 2, 1};
-    int n = 5;
-    int cnt = numberOfInversions(a, n);
-    cout << "The number of inversions are: "
-         << cnt << endl;
-    return 0;
-}
-
-
+};
